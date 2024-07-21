9e13ad122734fce810b5b81c52faa3b5b6d5275b
edsoncordeirodapaixao <edsonpaixao038@gmail.com>
2024-07-21 16:32:39 +0100

    Devclub tesla

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+ 
+    formulario.style.left = "41%"
+}
+
+function desaparecerFormulario(){
+
+    formulario.style.left = "-50%"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
